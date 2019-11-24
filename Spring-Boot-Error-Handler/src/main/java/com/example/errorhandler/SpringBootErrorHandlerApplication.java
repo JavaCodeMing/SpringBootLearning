26cff44457d686d4f82b75f5bd676ec6605df811
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2019-11-24 17:27:25 +0800

    新增SpringBoot统一异常处理之ErrorHandler

---
+++ Spring-Boot-Error-Handler/src/main/java/com/example/errorhandler/SpringBootErrorHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.errorhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootErrorHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootErrorHandlerApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot-Error-Handler/src/test/java/com/example/errorhandler/SpringBootErrorHandlerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.errorhandler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootErrorHandlerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
