707a74a950ffdda014d91cafa330d86c982589a9
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-01-02 13:01:17 +0800

    添加消息的确认和拒绝

--- Spring-Boot-Rabbit-Reliable-Delivery/Rabbitmq-Customer/src/main/java/com/example/customer/config/RabbitMqConfig.java
+++ Spring-Boot-Rabbit-Reliable-Delivery/Rabbitmq-Customer/src/main/java/com/example/customer/config/RabbitMqConfig.java
@@ -1,5 +1,6 @@
 package com.example.customer.config;
 
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
@@ -12,11 +13,15 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RabbitMqConfig {
+
     @Bean("jsonListenerFactory")
     public RabbitListenerContainerFactory jsonListenerFactory(ConnectionFactory connectionFactory){
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
+        //设置消费者的消息确认模式: MANUAL模式
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
         factory.setMessageConverter(new Jackson2JsonMessageConverter());
         return factory;
     }
+
 }
--- Spring-Boot-Rabbit-Reliable-Delivery/Rabbitmq-Customer/src/main/java/com/example/customer/consumer/OrderReceiver.java
+++ Spring-Boot-Rabbit-Reliable-Delivery/Rabbitmq-Customer/src/main/java/com/example/customer/consumer/OrderReceiver.java
@@ -1,14 +1,19 @@
 package com.example.customer.consumer;
 
 import com.example.common.domain.Order;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class OrderReceiver {
@@ -22,9 +27,20 @@ public class OrderReceiver {
                     key="order.*"
             )
     )
-    public void onOrderMessage(@Payload Order order) throws Exception {
-        logger.info("----收到消息，开始消费-----");
-        logger.info("订单id："+order.getId());
-        logger.info("--------消费完成--------");
+    public void onOrderMessage(@Payload Order order, @Headers Map<String, Object> headers, Channel channel) throws Exception {
+        try {
+            logger.info("----收到消息，开始消费-----");
+            logger.info("订单id："+order.getId());
+            //消息确认,(deliveryTag,multiple:是否确认所有消息)
+            channel.basicAck((Long) headers.get(AmqpHeaders.DELIVERY_TAG), false);
+            logger.info("--------消费完成--------");
+        }catch (Exception e){
+            //消息拒绝(deliveryTag,multiple,requeue:拒绝后是否重新回到队列)
+            channel.basicNack((Long) headers.get(AmqpHeaders.DELIVERY_TAG), false, false);
+            // 拒绝一条
+            // channel.basicReject();
+            logger.info("--------消息消费异常--------");
+        }
+
     }
 }
