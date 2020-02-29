47bf09f785a73690a356c4be04a02f3571f5dd5f
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-02-29 22:16:35 +0800

    升级版本及更新文档

--- Spring-Boot-JdbcTemplate/src/main/java/com/example/jdbctemplate/mapper/impl/StudentMapperImpl.java
+++ Spring-Boot-JdbcTemplate/src/main/java/com/example/jdbctemplate/mapper/impl/StudentMapperImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.sql.Types;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by dengzhiming on 2019/3/24
@@ -30,7 +31,7 @@ public class StudentMapperImpl implements StudentMapper {
         // return this.jdbcTemplate.update(sql, args, argTypes);
         // 较多字段使用: NamedParameterJdbcTemplate; 返回结果,可直接使用List<Map<String, Object>>来接收
         String sql = "insert into student(sno,sname,ssex) values(:sno,:name,:sex)";
-        NamedParameterJdbcTemplate npjt = new NamedParameterJdbcTemplate(this.jdbcTemplate.getDataSource());
+        NamedParameterJdbcTemplate npjt = new NamedParameterJdbcTemplate(Objects.requireNonNull(this.jdbcTemplate.getDataSource()));
         return npjt.update(sql, new BeanPropertySqlParameterSource(student));
     }
 
@@ -62,7 +63,7 @@ public class StudentMapperImpl implements StudentMapper {
         Object[] args = {sno};
         int[] argTypes = {Types.VARCHAR};
         List<Student> studentList = this.jdbcTemplate.query(sql, args, argTypes, new StudentObj());
-        if (studentList != null && studentList.size() > 0) {
+        if (studentList.size() > 0) {
             return studentList.get(0);
         } else {
             return null;
