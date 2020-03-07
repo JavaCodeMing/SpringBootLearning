771761242c2eb6a65a84dcd04bd0eb6c6c4368e9
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-03-07 17:05:39 +0800

    删除idea自动生成无用代码并更新版本

---
+++ Spring-Boot-Admin/Spring-Boot-Admin-Server/src/main/java/com/example/server/SpringBootAdminServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot-Admin/Spring-Boot-Admin-Server/src/test/java/com/example/server/SpringBootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
