a9ab6f3143ac323d7e83eefb30eb57d8a8090e6f
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-03-07 16:37:13 +0800

    更新版本

--- Spring-Boot-Admin/Spring-Boot-Admin-Client/src/test/java/com/example/client/SpringBootAdminClientApplicationTests.java
+++ Spring-Boot-Admin/Spring-Boot-Admin-Client/src/test/java/com/example/client/SpringBootAdminClientApplicationTests.java
@@ -1,16 +1,13 @@
 package com.example.client;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootAdminClientApplicationTests {
+class SpringBootAdminClientApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
