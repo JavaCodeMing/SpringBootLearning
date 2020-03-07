275834f5153b196bc2a66e8607e59c82c7cd9194
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-03-08 00:41:58 +0800

    删除idea自动生成无用代码并更新版本

--- Spring-Boot-Swaggerdoc/src/test/java/com/example/swaggerdoc/SpringBootSwaggerdocApplicationTests.java
+++ Spring-Boot-Swaggerdoc/src/test/java/com/example/swaggerdoc/SpringBootSwaggerdocApplicationTests.java
@@ -1,14 +1,11 @@
 package com.example.swaggerdoc;
 
 import com.example.swaggerdoc.util.SwaggerUtils;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.MalformedURLException;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootSwaggerdocApplicationTests {
 
