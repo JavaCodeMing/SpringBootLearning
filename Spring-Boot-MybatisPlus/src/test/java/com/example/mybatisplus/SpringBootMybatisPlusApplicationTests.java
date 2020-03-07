cf797422ed94017607407077d9e18585c5f93f19
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-03-07 23:12:52 +0800

    删除idea自动生成无用代码并更新版本

--- Spring-Boot-MybatisPlus/src/test/java/com/example/mybatisplus/SpringBootMybatisPlusApplicationTests.java
+++ Spring-Boot-MybatisPlus/src/test/java/com/example/mybatisplus/SpringBootMybatisPlusApplicationTests.java
@@ -10,16 +10,13 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.example.mybatisplus.entity.User;
 import com.example.mybatisplus.mapper.UserMapper;
 import com.example.mybatisplus.service.UserService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootMybatisPlusApplicationTests {
 
