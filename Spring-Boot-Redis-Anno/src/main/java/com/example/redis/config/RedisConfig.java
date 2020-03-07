2c70d1e24d115d45a4a27e663dfae3e1845a611e
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-03-08 00:23:58 +0800

    删除idea自动生成无用代码并更新版本

--- Spring-Boot-Redis-Anno/src/main/java/com/example/redis/config/RedisConfig.java
+++ Spring-Boot-Redis-Anno/src/main/java/com/example/redis/config/RedisConfig.java
@@ -88,7 +88,6 @@ public class RedisConfig extends CachingConfigurerSupport {
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
         template.setValueSerializer(jackson2JsonRedisSerializer);
     }
