20de6d7e67b7a0ca93ab175eb6f85f947f9e5363
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-03-07 15:54:03 +0800

    修改项目结构

--- Spring-Boot-ActiveMQ/src/test/java/com/example/activemq/SpringBootActiveMqApplicationTests.java
+++ Spring-Boot-ActiveMQ/src/test/java/com/example/activemq/SpringBootActiveMqApplicationTests.java
@@ -1,16 +1,13 @@
 package com.example.activemq;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootActiveMqApplicationTests {
+class SpringBootActiveMqApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
