a22f2561fe8e8a762fd08d5568d240b03d5a19e9
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-03-08 12:40:44 +0800

    删除idea自动生成无用代码并更新版本

--- Spring-Boot-XSSFilter/src/test/java/com/example/xssfilter/SpringBootXssFilterApplicationTests.java
+++ Spring-Boot-XSSFilter/src/test/java/com/example/xssfilter/SpringBootXssFilterApplicationTests.java
@@ -1,12 +1,9 @@
 package com.example.xssfilter;
 
 import com.example.xssfilter.utils.JsoupUtil;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootXssFilterApplicationTests {
 
